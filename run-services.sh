00c5bd9d94af6c94ad93489f3013af77441edd8d
Essa Mamdani <mamdani.info@gmail.com>
2024-06-05 09:01:49 +0500

    First Init

---
+++ run-services.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Run SearXNG
+docker run -d --name searxng -p 8888:8888 searxng/searxng
+
+# Run Browserless
+docker run -d --name browserless -p 3000:3000 browserless/chrome
+
+echo "SearXNG is running at http://localhost:8888"
+echo "Browserless is running at http://localhost:3000"
